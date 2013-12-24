64a0bd49c7b9af2089129deb8e7831566cc2c17b
Si Beaumont <simon.beaumont@citrix.com>
2013-12-24 16:29:11 +0000

    Add ribbon link to tamarind trophy page

---
+++ tamarind-trophy/ribbon.js
@@ -0,0 +1,1 @@
+document.write('<a href="/tamarind-trophy" target=""><img src="/tamarind-trophy/ribbon.gif" border="0" width="150" height="150" style="position:absolute; right:0px; top:0px; margin:0px; padding:0px"></a>')
